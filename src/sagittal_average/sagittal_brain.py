d7969c0eec01e42d70730e5c83c294bca129087b
Jannelle <zcemjof@ucl.ac.uk>
2025-11-30 14:03:14 +0000

    Corrected sagittal_brain and pyproject.toml for successful package install

--- src/sagittal_average/sagittal_brain.py
+++ src/sagittal_average/sagittal_brain.py
@@ -1,9 +1,13 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 import numpy as np
+from pathlib import Path
 
+data_dir = Path(__file__).resolve().parent / "data"
+default_input = data_dir / "brain_sample.csv"
+default_output = data_dir / "brain_average.csv"
 
-def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv'):
+def run_averages(file_input= default_input, file_output= default_output):
     """
     Calculates the average through the coronal planes
     The input file should has as many columns as coronal planes
@@ -25,9 +29,9 @@ def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv')
 if __name__ == "__main__":
     parser = ArgumentParser(description="Calculates the average for each sagittal-horizontal plane.",
                             formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('file_input', nargs='?', default="brain_sample.csv",
+    parser.add_argument('file_input', nargs='?', default= default_input,
                         help="Input CSV file with the results from scikit-brain binning algorithm.")
-    parser.add_argument('--file_output', '-o', default="brain_average.csv",
+    parser.add_argument('--file_output', '-o', default= default_output,
                         help="Name of the output CSV file.")
     arguments = parser.parse_args()
 
--- tests/test_sagittal_brain.py
+++ tests/test_sagittal_brain.py
@@ -1,8 +1,23 @@
 import numpy as np
+import sys
+from sagittal_average import run_averages
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
+tests = Path(__file__).parent 
 
 input = np.zeros((20, 20))
 input[-1, :] = 1
 np.savetxt("brain_sample.csv", input, fmt='%d', delimiter=',')
 
-expected = [0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+# The expected result is all zeros, except the last one, it should be 1
+expected = np.zeros(20)
+expected[-1] = 1
+
+run_averages(file_input="brain_sample.csv",
+             file_output="brain_average.csv")
+
+result = np.loadtxt(tests / "brain_average.csv",  delimiter=',')
+np.testing.assert_array_equal(result, expected)
+
 
