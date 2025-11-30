c3b3a90ae6736f535f3a29d56130b2674f83dbae
Jannelle <zcemjof@ucl.ac.uk>
2025-11-30 14:22:09 +0000

    Ignores metadeta associated with sagittal_brain.py

--- src/sagittal_average/sagittal_brain.py
+++ src/sagittal_average/sagittal_brain.py
@@ -22,7 +22,6 @@ def run_averages(file_input= default_input, file_output= default_output):
     # and makes it as a row vector
     # averages = planes.mean(axis=0)[np.newaxis, :]
     averages = planes.mean(axis=1)
-    ''
 
     # write it out on my file
     np.savetxt(file_output, averages, fmt='%.1f', delimiter=',')
